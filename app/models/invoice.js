a7509b218e69a9f7e1911fb3c747dffbf0437b15
Denis Peplin <denis.peplin@gmail.com>
2014-11-24 11:34:58 +0300

    Add InvoiceProduct as hasMany to Invoice. Display in loop.

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   created_at: DS.attr('date'),
-  received: DS.attr('boolean')
+  received: DS.attr('boolean'),
+  invoiceProducts: DS.hasMany('invoice-product')
 });
