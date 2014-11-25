ea92d987b9f3c3caa543d05612f4f515f4b415c3
Denis Peplin <denis.peplin@gmail.com>
2014-11-25 12:48:25 +0300

    Add products reserving. Expect server to add them to order.

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  actions: {
+    reserve: function() {
+      var self = this;
+      this.get('model').save().then(function () {
+      }, function (response) {
+        self.set('errors', response.responseJSON.errors);
+      });
+    }
+  },
   totalPrice: function() {
     return (this.get('price') * this.get('reserve')).toFixed(2);
-  }.property('price', 'reserve')
+  }.property('price', 'reserve'),
+  notReserving: function() {
+    return this.get('reserve') < 1;
+  }.property('reserve')
 });
