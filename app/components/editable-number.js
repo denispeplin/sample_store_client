f48bbc33b97598e8f4e60cd0210679506793abc5
Denis Peplin <denis.peplin@gmail.com>
2014-11-27 16:09:01 +0300

    Don't allow to edit placed bids.

--- app/components/editable-number.js
+++ app/components/editable-number.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showInput: Ember.computed.and('editable', 'isEditing'),
   actions: {
     toggleEditing: function() {
       this.toggleProperty('isEditing');
--- app/controllers/bid-product.js
+++ app/controllers/bid-product.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  bidActive: Ember.computed.not('bid.placed'),
   actions: {
     save: function() {
       var self = this;
